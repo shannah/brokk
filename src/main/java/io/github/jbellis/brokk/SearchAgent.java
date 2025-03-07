08ee5a328b9cbd2dd326749391b0048cdf6f0a87
Jonathan Ellis <jbellis@gmail.com>
2025-03-07 14:09:51 -0600

    tweak prompts

--- src/main/java/io/github/jbellis/brokk/SearchAgent.java
+++ src/main/java/io/github/jbellis/brokk/SearchAgent.java
@@ -390,14 +390,15 @@ public class SearchAgent {
         %s>
         </query>
         
-        Determine the next tool(s) to call to search for code related to the query.
+        Determine the next tool to call to search for code related to the query.
         Round trips are expensive! If you have multiple search terms to learn about, group them in a single call.
         Of course, `abort` and `answer` tools cannot be composed with others.
         Each tool call must include a `learnings` parameter that records what you learned
-        from the MOST RECENT action. This will be the ONLY content from that action saved in
-        history, so make it comprehensive!
+        from the MOST RECENT action in enough detail that you never have to repeat that action
+        This will be the ONLY content from that action saved in history, you will not see the full response again,
+        so make it comprehensive!
           - Bad: Found several classes and methods related to the query
-          - Good: Found classes org.foo.bar.X and org.foo.baz.Y, and methods and org.foo.qux.Z.method1 and org.foo.fizz.W.method2 related to the query
+          - Good: Found classes org.foo.bar.X and org.foo.baz.Y, and methods org.foo.qux.Z.method1 and org.foo.fizz.W.method2 related to the query
         """.formatted(query);
         if (symbolsFound) {
             // Switch to beast mode if we're running out of time
