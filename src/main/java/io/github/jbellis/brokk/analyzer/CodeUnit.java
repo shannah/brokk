df9fbcfef16b3981d85bc8d67df66fc91c894230
Jonathan Ellis <jbellis@gmail.com>
2025-03-23 09:26:33 -0500

    refactor: Replace Scala CodeUnit with Java implementation

---
+++ src/main/java/io/github/jbellis/brokk/analyzer/CodeUnit.java
@@ -0,0 +1,136 @@
+package io.github.jbellis.brokk.analyzer;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a named code element (class, function, or field).
+ */
+public record CodeUnit(CodeUnitType kind, String fqName)
+        implements Comparable<CodeUnit>, Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return just the last symbol name (a.b.C -> C, a.b.C.foo -> foo)
+     */
+    public String name()
+    {
+        var lastDotIndex = fqName.lastIndexOf('.');
+        return lastDotIndex == -1
+                ? fqName
+                : fqName.substring(lastDotIndex + 1);
+    }
+
+    /**
+     * @return for classes: just the class name
+     *         for functions and fields: className.memberName (last two components)
+     */
+    public String shortName()
+    {
+        var parts = fqName.split("\\.");
+        return switch (kind)
+        {
+            case CLASS -> parts[parts.length - 1];
+            default ->
+            {
+                if (parts.length >= 2)
+                    yield parts[parts.length - 2] + "." + parts[parts.length - 1];
+                yield parts[parts.length - 1];
+            }
+        };
+    }
+
+    public boolean isClass()
+    {
+        return kind == CodeUnitType.CLASS;
+    }
+
+    public boolean isFunction()
+    {
+        return kind == CodeUnitType.FUNCTION;
+    }
+
+    /**
+     * @return the package portion of the fully qualified name,
+     *         excluding whatever shortName() would return.
+     */
+    public String packageName()
+    {
+        return switch (kind)
+        {
+            case CLASS ->
+            {
+                var lastDotIndex = fqName.lastIndexOf('.');
+                if (lastDotIndex == -1)
+                    yield "";
+                yield fqName.substring(0, lastDotIndex);
+            }
+            default ->
+            {
+                var parts = fqName.split("\\.");
+                if (parts.length <= 2)
+                    yield "";
+                yield Arrays.stream(parts, 0, parts.length - 2)
+                        .collect(Collectors.joining("."));
+            }
+        };
+    }
+
+    @Override
+    public int compareTo(CodeUnit other)
+    {
+        return this.fqName.compareTo(other.fqName);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (!(obj instanceof CodeUnit other)) return false;
+        return Objects.equals(this.fqName, other.fqName);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(fqName);
+    }
+
+    @Override
+    public String toString()
+    {
+        return switch (kind)
+        {
+            case CLASS -> "CLASS[" + fqName + "]";
+            case FUNCTION -> "FUNCTION[" + fqName + "]";
+            case FIELD -> "FIELD[" + fqName + "]";
+        };
+    }
+
+    /**
+     * Factory method to create a CodeUnit of type CLASS.
+     */
+    public static CodeUnit cls(String reference)
+    {
+        return new CodeUnit(CodeUnitType.CLASS, reference);
+    }
+
+    /**
+     * Factory method to create a CodeUnit of type FUNCTION.
+     */
+    public static CodeUnit fn(String reference)
+    {
+        return new CodeUnit(CodeUnitType.FUNCTION, reference);
+    }
+
+    /**
+     * Factory method to create a CodeUnit of type FIELD.
+     */
+    public static CodeUnit field(String reference)
+    {
+        return new CodeUnit(CodeUnitType.FIELD, reference);
+    }
+}
