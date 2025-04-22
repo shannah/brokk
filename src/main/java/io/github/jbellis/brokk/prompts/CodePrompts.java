f0e03ca81ba59502dfbacde03ada145a6b3a711b
Jonathan Ellis <jbellis@gmail.com>
2025-04-21 21:31:12 -0500

    fix: match EBP with EBP

--- src/main/java/io/github/jbellis/brokk/prompts/CodePrompts.java
+++ src/main/java/io/github/jbellis/brokk/prompts/CodePrompts.java
@@ -45,11 +45,11 @@ public abstract class CodePrompts {
         var messages = new ArrayList<ChatMessage>();
 
         messages.add(new SystemMessage(formatIntro(cm, reminder)));
-        messages.addAll(EditBlockConflictsParser.instance.exampleMessages());
+        messages.addAll(EditBlockParser.instance.exampleMessages());
         messages.addAll(cm.getHistoryMessages());
         messages.addAll(sessionMessages);
         messages.addAll(cm.getWorkspaceContentsMessages(false));
-        messages.add(new UserMessage(EditBlockConflictsParser.instance.instructions(reminder)));
+        messages.add(new UserMessage(EditBlockParser.instance.instructions(reminder)));
         messages.add(new AiMessage("I will format my edits accordingly."));
 
         return messages;
