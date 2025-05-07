28b240d53fe668695175bef0da45420e36986b98
Jonathan Ellis <jbellis@gmail.com>
2025-05-06 22:18:05 -0500

    simplify

--- src/main/resources/treesitter/javascript.scm
+++ src/main/resources/treesitter/javascript.scm
@@ -6,30 +6,13 @@
 ((function_declaration
   name: (identifier) @function.name) @function.definition)
 
-; Exported class
-(export_statement
-  declaration: ((class_declaration
-    name: (identifier) @class.name) @class.definition))
-
-; Exported function
-(export_statement
-  declaration: ((function_declaration
-    name: (identifier) @function.name) @function.definition))
-
 ; Top-level const/let/var MyComponent = () => { ... }
 ((lexical_declaration
   (variable_declarator
     name: (identifier) @function.name
     value: ((arrow_function) @function.definition))))
 
-; Exported const/let/var MyComponent = () => { ... }
-(export_statement
-  declaration: (lexical_declaration
-                 (variable_declarator
-                   name: (identifier) @function.name
-                   value: ((arrow_function) @function.definition))))
-
-; Class method (non-exported class)
+; Class method
 ; Captures method_definition within a class_body. Name can be property_identifier or identifier.
 (
   (class_declaration
@@ -38,20 +21,6 @@
               name: [(property_identifier) (identifier)] @function.name
             ) @function.definition
           )
-  )
-)
-
-; Class method (exported class)
-; Captures method_definition within an exported class_body.
-(
-  (export_statement
-    declaration: (class_declaration
-                   body: (class_body
-                           (method_definition
-                             name: [(property_identifier) (identifier)] @function.name
-                           ) @function.definition
-                         )
-                 )
   )
 )
 
