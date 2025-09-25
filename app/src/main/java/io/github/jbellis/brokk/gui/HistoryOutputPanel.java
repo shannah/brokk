1b12292735d1817037c10fd930f2a4a146d87bd1
Jonathan Ellis <jbellis@gmail.com>
2025-09-25 15:14:19 -0500

    UI: Improve task count label in HistoryOutputPanel

--- app/src/main/java/io/github/jbellis/brokk/gui/HistoryOutputPanel.java
+++ app/src/main/java/io/github/jbellis/brokk/gui/HistoryOutputPanel.java
@@ -1770,7 +1770,9 @@ public class HistoryOutputPanel extends JPanel {
             timeLabel.setText(formatModified(value.modified()));
 
             var cnt = sessionAiResponseCounts.get(value.id());
-            countLabel.setText(cnt != null ? cnt + " tasks" : "");
+            countLabel.setText(cnt != null
+                    ? String.format("%d %s", cnt, cnt == 1 ? "task" : "tasks")
+                    : "");
             if (cnt == null) {
                 triggerAiCountLoad(value);
             }
