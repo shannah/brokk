211847c77487107c89cab64c2ac676dcd1587061
Steve Hannah <steve@weblite.ca>
2025-10-01 17:26:52 -0700

    spotlessApply

--- app/src/main/java/io/github/jbellis/brokk/gui/util/JDeploySettingsUtil.java
+++ app/src/main/java/io/github/jbellis/brokk/gui/util/JDeploySettingsUtil.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
