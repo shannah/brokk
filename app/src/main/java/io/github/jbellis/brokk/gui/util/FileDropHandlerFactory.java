e74ccb5b306402b764a94642c78021c57cd8bd9e
Jonathan Ellis <jbellis@gmail.com>
2025-10-18 13:47:25 -0500

    UI: Remove file drop edit-or-summarize dialog; add for edit instead

--- app/src/main/java/io/github/jbellis/brokk/gui/util/FileDropHandlerFactory.java
+++ app/src/main/java/io/github/jbellis/brokk/gui/util/FileDropHandlerFactory.java
@@ -1,15 +1,11 @@
 package io.github.jbellis.brokk.gui.util;
 
-import io.github.jbellis.brokk.AnalyzerWrapper;
-import io.github.jbellis.brokk.ContextManager;
 import io.github.jbellis.brokk.IConsoleIO;
 import io.github.jbellis.brokk.analyzer.ProjectFile;
 import io.github.jbellis.brokk.gui.Chrome;
-import io.github.jbellis.brokk.gui.dialogs.DropActionDialog;
 import java.awt.datatransfer.DataFlavor;
 import java.io.File;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
@@ -22,17 +18,6 @@ public final class FileDropHandlerFactory {
     private static final Logger logger = LogManager.getLogger(FileDropHandlerFactory.class);
 
     private FileDropHandlerFactory() {}
-
-    private static boolean isAnalyzerReady(ContextManager contextManager, IConsoleIO io) {
-        if (!contextManager.getAnalyzerWrapper().isReady()) {
-            io.systemNotify(
-                    AnalyzerWrapper.ANALYZER_BUSY_MESSAGE,
-                    AnalyzerWrapper.ANALYZER_BUSY_TITLE,
-                    JOptionPane.INFORMATION_MESSAGE);
-            return false;
-        }
-        return true;
-    }
 
     public static TransferHandler createFileDropHandler(Chrome chrome) {
         var contextManager = chrome.getContextManager();
@@ -90,45 +75,8 @@ public final class FileDropHandlerFactory {
                         return false;
                     }
 
-                    // Ask the user what to do
-                    var analyzedExts = contextManager.getProject().getAnalyzerLanguages().stream()
-                            .flatMap(lang -> lang.getExtensions().stream())
-                            .collect(Collectors.toSet());
-                    boolean canSummarize = projectFiles.stream().anyMatch(pf -> analyzedExts.contains(pf.extension()));
-                    java.awt.Point pointer = null;
-                    try {
-                        var pi = java.awt.MouseInfo.getPointerInfo();
-                        if (pi != null) {
-                            pointer = pi.getLocation();
-                        }
-                    } catch (Exception ignore) {
-                        // ignore
-                    }
-                    var selection = DropActionDialog.show(chrome.getFrame(), canSummarize, pointer);
-                    if (selection == null) {
-                        chrome.showNotification(IConsoleIO.NotificationRole.INFO, "Drop canceled");
-                        return false;
-                    }
-                    switch (selection) {
-                        case EDIT -> {
-                            contextManager.submitContextTask(() -> {
-                                contextManager.addFiles(projectFiles);
-                            });
-                        }
-                        case SUMMARIZE -> {
-                            if (!isAnalyzerReady(contextManager, chrome)) {
-                                return false;
-                            }
-                            contextManager.submitContextTask(() -> {
-                                contextManager.addSummaries(
-                                        new java.util.HashSet<>(projectFiles), Collections.emptySet());
-                            });
-                        }
-                        default -> {
-                            logger.warn("Unexpected drop selection: {}", selection);
-                            return false;
-                        }
-                    }
+                    // Immediately add dropped files as editable (no dialog)
+                    contextManager.submitContextTask(() -> contextManager.addFiles(projectFiles));
 
                     return true;
                 } catch (Exception ex) {
